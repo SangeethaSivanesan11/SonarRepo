8f719d5d55ddbf4ad4590afc775f64ebbc240b60
sangeetha.sivanesan <sangeetha.sivanesan@zohocorp.com>
2018-06-18 16:26:56 +0530

    initial commit

---
+++ FileRead.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class FileRead
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<=1000000000;i++){
+ 	try{
+		File file = new File("binarylogs","sangeetha.txt");
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line = "", oldtext = "";
+		while((line = reader.readLine()) != null){
+			oldtext += line + "\r\n";
+                        System.out.println(oldtext);
+		}
+	}catch (IOException ioe){
+		ioe.printStackTrace();
+	}
+        }
+    }
+}
